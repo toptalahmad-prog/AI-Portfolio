f17dbc61370a7b492576a4f4b46d755e8f94fd11
toptalahmad-prog <toptal.ahmad@gmail.com>
2026-03-28 03:06:14 +0500

    Fix: Add missing make_response import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory, session, redirect, render_template_string
+from flask import Flask, request, jsonify, send_from_directory, session, redirect, render_template_string, make_response
 import requests
 import os
 import time
